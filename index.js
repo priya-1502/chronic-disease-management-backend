4cd702535fcac9c0fcaebd2b50edfd8af29ac1fa
Hariharasudhaan S <hsudhaan@gmail.com>
2025-03-08 12:17:33 +0530

    Added folder structure

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 //App config
 const app = express()
 const port = process.env.PORT || 3001
-// const connection_URL = 'mongodb+srv://admin:hFD58CRomLtUonOK@cluster0.6xtbqnw.mongodb.net/?retryWrites=true&w=majority'
+const connection_URL = process.env.DB_CONNECTION_STRING
+
+
+mongoose.connect(connection_URL).then((res)=>console.log("DB Connected")).catch((err)=>console.log(err))
 
 //Listener
 app.listen(port,()=> console.log(`Listerning on localhost : ${port}`))
 
 
-app.get('/',(req,res)=> res.status(200).send("Hello Hari World"));
+app.get('/',(req,res)=> res.status(200).send("Hello Hari World"))
 
 
